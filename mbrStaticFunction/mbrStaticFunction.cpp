39be5e1063644f2bf9c8edd359325bdca47726d9
Jodinurf <jodinurfarkhani04@gmail.com>
2023-05-17 13:51:12 +0700

    membuat static dengan tipe data integer pada fungsi getNim yang mempunyai mendefinisikan fungsi return pada nim

--- mbrStaticFunction/mbrStaticFunction.cpp
+++ mbrStaticFunction/mbrStaticFunction.cpp
@@ -14,5 +14,7 @@ public:
 	void printAll();
 
 	static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
-	
+	static int getNim() { return nim; /*Definisi Function*/ }
+
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
